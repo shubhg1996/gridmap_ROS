78714ff2a0dfaf854c9ad97c6104de2db69290c8
Shubh <shubhg1996@gmail.com>
2017-10-02 21:53:36 +0530

    service client

--- gmap2.cpp
+++ gmap2.cpp
@@ -4,18 +4,27 @@
 #include <string>
 #include <cmath>
 #include <limits>
+#include "classify/classify.h"
+#include <cv_bridge/cv_bridge.h>
 
 using namespace grid_map;
 float cammat[3][3] = {{1.0, 0.0, 1.0},{0.0, 1.0, 1.0},{0.0, 0.0, 1.0}}; 
 
+struct Retclass {
+  int type;
+  float prob;
+};
+
+// Initialize node and publisher.
+ros::init(argc, argv, "grid_map_worker");
+ros::NodeHandle nh("~");
+ros::ServiceClient client = n.serviceClient<classify::classify>("classify");
+imageSubscriber_ = nodeHandle_.subscribe("/image", 1, &imageCallback, this);
+ros::Publisher publisher = nh.advertise<grid_map_msgs::GridMap>("grid_map", 1, true);
+
+
 int main(int argc, char** argv)
 {
-  // Initialize node and publisher.
-  ros::init(argc, argv, "grid_map_worker");
-  ros::NodeHandle nh("~");
-  imageSubscriber_ = nodeHandle_.subscribe("/image", 1, &imageCallback, this);
-  ros::Publisher publisher = nh.advertise<grid_map_msgs::GridMap>("grid_map", 1, true);
-
   // Create grid map.
   GridMap map({"occmap"});
   map.setFrameId("map");
@@ -41,13 +50,18 @@ int main(int argc, char** argv)
   return 0;
 }
 
-//cluster output
-int cluster(cv::Mat roi) {
-  return out;
+//call python service for classifying
+Retclass cluster(cv::Mat roi) {
+  classify::classify srv;
+  cv_bridge::CvImagePtr roi_ptr;
+  roi_ptr->image = roi;
+  srv.request.roi = roi_ptr->toImageMsg();
+  client.call(srv);
+  return {srv.response.class,srv.response.prob};
 }
 
 //project imagepoint and objectness calculation
-int findObj(Position imgpos, std::vector<Point3f> tvec, std::vector<Point3f> rvec, cv::Mat img) {
+Retclass findObj(Position imgpos, std::vector<Point3f> tvec, std::vector<Point3f> rvec, cv::Mat img) {
   std::vector<Point3f> imgpt;
   std::vector<Point2f> imgout;
   imgpt.push_back(Point3f(imgpos.x(),imgpos.y(),0.0));
@@ -56,15 +70,16 @@ int findObj(Position imgpos, std::vector<Point3f> tvec, std::vector<Point3f> rve
   cv::Rect region_of_interest = Rect(imgout[0].x-5, imgout[0].y-5, 10, 10);
   cv::Mat image_roi = img(region_of_interest);
   //get cluster class
-  out = cluster(image_roi);
-  return out;
+  return cluster(image_roi);
 }
 
 
 //Image callback for gridmacp generation
-void ImageToGridmapDemo::imageCallback(const sensor_msgs::Image& msg)
+void imageCallback(const sensor_msgs::Image& msg)
 {
+  cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg);
   // image submap (iterators).
+  //get rvec, tvec
   //update x, y, theta from camera pose
   Position topLeftCorner(x-0.1*(std::cos(theta)+std::sin(theta)), y+0.1*(std::cos(theta)-std::sin(theta)));
   Position topRightCorner(x+0.1*(std::cos(theta)-std::sin(theta)), y+0.1*(std::cos(theta)+std::sin(theta)));
@@ -84,7 +99,9 @@ void ImageToGridmapDemo::imageCallback(const sensor_msgs::Image& msg)
         map.getPosition(*it2, imgpt);
         //convert to camera pixels and extract region
         //calculate objectness measure
-        curobj = findObj(imgpt);
+        Retclass out;
+        out = findObj(imgpt,rvec,tvec,cv_ptr->image);
+        curobj = out.type;
         //mark if lane
         if (curobj == 1) 
           map.at("occmap", *it2) = curobj;
